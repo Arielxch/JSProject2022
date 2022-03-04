e2f6dd21745f7b10168d7a7d81991011605b83c7
Arielxch <xinccheng@gmail.com>
2022-03-03 23:57:31 -0500

    markdown-blog accomplished

--- JS-Reviews/app.js
+++ JS-Reviews/app.js
@@ -54,6 +54,36 @@ let currentItem = 0;
 
 // load initial item
 
-window.addEventListener('DOMContenLoaded', function ()){
+window.addEventListener('DOMContenLoaded', function () {
+  showPerson(currentItem);
+});
 
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
 }
+
+// show next person
+
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// show previous person
+
+prevBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
